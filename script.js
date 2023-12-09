036bdd993c8a3336bc8935f044ded7ff0cb12741
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-12-09 23:02:43 +0700

    navbar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 if (outerWidth > 768) {
     alert('This Web Is Not Available On Desktop');
 }
+
+const navEl = document.querySelector(".nav");
+
+      window.addEventListener("scroll", () => {
+        if (window.scrollY >= 256) {
+          navEl.classList.add("nav-scrolled");
+          console.log("scrolled");
+        } else if (window.scrollY < 256) {
+          navEl.classList.remove("nav-scrolled");
+        }
+      });
