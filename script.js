4e7fcdc44a868ed9c7de4d57ca62d46fbe296f3e
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-12-09 23:05:23 +0700

    nav

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const navEl = document.querySelector(".nav");
       window.addEventListener("scroll", () => {
         if (window.scrollY >= 56) {
           navEl.classList.add("nav-scrolled");
-          console.log("scrolled");
         } else if (window.scrollY < 56) {
           navEl.classList.remove("nav-scrolled");
         }
