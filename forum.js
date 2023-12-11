6aa7e747a86c0a695fec198a45caa1d9e3ba91b1
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-12-11 17:32:34 +0700

    news

--- forum.js
+++ forum.js
@@ -12,18 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
         if (selectedForum) {
             selectedForum.classList.add('active');
         }
-
-        // Menghapus kelas 'active' dari semua tautan navigasi
-        var navLinks = document.querySelectorAll('.forum-nav a');
-        navLinks.forEach(function(link) {
-            link.classList.remove('active');
-        });
-
-        // Menambahkan kelas 'active' ke tautan navigasi yang dipilih
-        var selectedNavLink = document.querySelector('.forum-nav a[href="javascript:void(0);"][onclick="loadForum(\'' + forumId + '\')"]');
-        if (selectedNavLink) {
-            selectedNavLink.classList.add('active');
-        }
     };
 
     // Initial load (you can set the default forum to load here)
